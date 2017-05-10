10eaba8f02fc5547cd019f785f830c02ce2b1eea
Niko Laskaris <laskarisnik@gmail.com>
2017-05-10 13:27:09 -0400

    add photo and navbar transparency scroll feature, plus added new bio

--- js/main.js
+++ js/main.js
@@ -38,6 +38,24 @@ $(document).ready(function () {
 
     }
   });
+
+  // Navbar transparency scroll 
+  function checkScroll(){
+    var startY = $('.navbar').height() * 5;
+
+    if($(window).scrollTop() > startY){
+        $('.navbar').addClass("scrolled");
+    }else{
+        $('.navbar').removeClass("scrolled");
+    }
+}
+
+  if($('.navbar').length > 0){
+    $(window).on("scroll load resize", function() {
+      checkScroll();
+    });
+  }
+
 });
 
 
